263500423c3778ca79a93f99a49c178c1b613d61
Francesco Alterio <francescoalteriog@gmail.com>
2022-06-23 12:08:15 -0400

    fix bug in screen

--- Screens.js
+++ Screens.js
@@ -47,7 +47,7 @@ export default function Screens() {
       // Action after the ad is closed]
       dispatch(resetTenTasks());
     }
-  }, [isClosed, navigation]);
+  }, [isClosed]);
 
   useEffect(() => {
     const admobAds = async () => {
